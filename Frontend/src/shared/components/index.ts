9d4d1a35f3a0150fbafad20fd7d229eadad54c6f
Nguyễn Văn Trung Ánh <153763811+hnagnurtme@users.noreply.github.com>
2025-12-30 18:27:52 +0700

    feat(main ui): enhance homepage layout and styling, add button components (#4)

--- Frontend/src/shared/components/index.ts
+++ Frontend/src/shared/components/index.ts
@@ -1,5 +1,4 @@
 export { default as Header} from "./Header";
 export { default as Footer} from "./Footer";
 export { default as Loader} from "./Loader";
-export { default as Button} from "./Button";
-export { default as InputField} from "./InputField";
+export * from "./Button";
